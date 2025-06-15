7512a653c6ab3f96702ac741e7211a46f130860d
Harsh Sultania <harshsultania@Harshs-MacBook-Air.local>
2025-06-15 15:24:07 +0530

    basic backend done

--- backend/index.js
+++ backend/index.js
@@ -6,12 +6,13 @@ import connectDB from "./utils/db.js";
 import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
-//import applicationRoute from "./routes/application.route.js";
+import applicationRoute from "./routes/application.route.js";
 
 dotenv.config({});
 
 const app = express();
 
+// middleware
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
@@ -24,10 +25,13 @@ app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
 
+
+// api's
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
-//app.use("/api/v1/application", applicationRoute);
+app.use("/api/v1/application", applicationRoute);
+
 
 
 app.listen(PORT,()=>{
